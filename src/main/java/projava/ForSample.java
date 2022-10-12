d48f65cbb8b8972a21cf7bac57a5599cfd18e0ed
TomozQ <inf.4clover@gmail.com>
2022-10-12 20:05:31 +0900

    for, while, continue, break

--- src/main/java/projava/ForSample.java
+++ src/main/java/projava/ForSample.java
@@ -2,8 +2,31 @@ package projava;
 
 public class ForSample {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println(i);
+        }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i ++;
+        }
+
+        // continueとbreak
+        for (int ii = 0; ii < 5; ii++) {
+            if( ii == 2) {
+                System.out.println("skip");
+                continue;
+            }
+            System.out.println(ii);
+        }
+
+        for (int iii = 0; iii < 5; iii++) {
+            if(iii == 2) {
+                System.out.println("finish");
+                break;
+            }
+            System.out.println(iii);
         }
     }
 }

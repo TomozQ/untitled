3b185487c97e259ef7b4643d2e05e8f9eea5814c
TomozQ <inf.4clover@gmail.com>
2022-10-22 16:20:19 +0900

    class extends

--- src/main/java/projava/InheritSample.java
+++ src/main/java/projava/InheritSample.java
@@ -1,5 +1,7 @@
 package projava;
 
+import java.util.List;
+
 public class InheritSample {
 
     /*
@@ -17,7 +19,49 @@ public class InheritSample {
         レコードはクラスを継承できない
      */
 
-    public static void main(String[] args) {
+    static class User {
+        String name;
 
+        public String getName() {
+            return name;
+        }
+    }
+
+    static class Student extends User {
+        int score;
+
+        Student(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        public int getScore () {
+            return score;
+        }
+    }
+
+    static class Teacher extends User {
+        String subject;
+
+        Teacher(String name, String subject) {
+            this.name = name;
+            this.subject = subject;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+    }
+    public static void main(String[] args) {
+        List<User> people = List.of(
+                new Student("kis", 80),
+                new Teacher("hosoya", "Math")
+        );
+
+        for(var p : people) {
+            System.out.println("こんにちは%sさん".formatted(p.getName()));
+        }
+        // こんにちはkisさん
+        // こんにちはhosoyaさん
     }
 }
